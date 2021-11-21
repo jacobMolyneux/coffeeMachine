130d22a0cb55ffd62d0a27db178b57859765065f
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-11-21 17:35:51 -0500

    machine continuosly runs

--- main.py
+++ main.py
@@ -6,21 +6,31 @@ coffee_menu = Menu()
 money_handler = MoneyMachine()
 barista = CoffeeMaker()
 # 1. get order
-order = input(
-    'What would you like to order? We make latte, espresso, cappuccino: ')
 
-# 2. check if resources are sufficient
-drink = coffee_menu.find_drink(order)
 
-if barista.is_resource_sufficient(drink) == True:
-    print('Okay i can make your drink!')
-    if money_handler.make_payment(1) == True:
-        barista.make_coffee(drink)
-    elif money_handler.make_payment(1) == False:
-        print('Oh No!')
+def runMachine():
+    order = input(
+        'What would you like to order? We make latte, espresso, cappuccino: ')
+    if order == 'report profit':
+        money_handler.report()
+    elif order == 'report':
+        barista.report()
+    else:
 
-else:
-    print("Sorry, I can't make your drink")
-# 3. process coins
+        # 2. check if resources are sufficient
+        drink = coffee_menu.find_drink(order)
 
-# 4. make drink
+        if barista.is_resource_sufficient(drink) == True:
+            print('Okay i can make your drink!')
+            if money_handler.make_payment(1) == True:
+                barista.make_coffee(drink)
+            elif money_handler.make_payment(1) == False:
+                print('Oh No!')
+
+        else:
+            print("Sorry, I can't make your drink")
+
+
+running = True
+while running == True:
+    runMachine()
